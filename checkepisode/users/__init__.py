4ac7fb654a4f89ff58d18b220af3955bb38f2cc1
Ondrej Fabry <truefurby@gmail.com>
2012-05-07 23:39:55 +0200

    added show/episode relationship

--- checkepisode/users/__init__.py
+++ checkepisode/users/__init__.py
@@ -3,13 +3,24 @@ from checkepisode.models import *
 from flask.ext.sqlalchemy import SQLAlchemy
 from passlib.apps import custom_app_context as pwd_context
 
+favorite_series = db.Table('favorite_series',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('series_id', db.Integer, db.ForeignKey('series.id'))
+)
+
+watched_episodes = db.Table('watched_episodes',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('episode_id', db.Integer, db.ForeignKey('episode.id'))
+)
 
 class User(db.Model):
-    __tablename__ = 'users'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(35), unique=True)
     password = db.Column(db.String(128))
+    favorite_series = db.relationship('Series', secondary=favorite_series,
+        backref=db.backref('users', lazy='dynamic'))
+    watched_episodes = db.relationship('Episode', secondary=watched_episodes,
+        backref=db.backref('users', lazy='dynamic'))
 
     def __init__(self, name, password):
         app.logger.info('Creating user "%s".', name)
