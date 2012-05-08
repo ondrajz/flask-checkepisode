2596d3e6ee22be7a3fead70e2fe8a56e2235d2c7
Ondrej Fabry <truefurby@gmail.com>
2012-05-08 22:45:31 +0200

    rebuild and addshows scripts merged

--- rebuild.py
+++ rebuild.py
@@ -1,3 +1,17 @@
-from checkepisode.models import db
+from checkepisode.models import db,Series,User
 db.drop_all()
 db.create_all()
+
+shows = open('shows','r')
+for line in shows.readlines():
+    tvName = line.strip('\n')
+    if len(tvName)>0:
+        print '%s' % tvName
+        new = Series(tvName)
+        db.session.add(new)
+shows.close()
+
+admin = User('admin', 'admin')
+db.session.add(admin)
+
+db.session.commit()
