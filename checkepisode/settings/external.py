78755529936c6baa04ec46962e628e0e2fb91402
Ondrej Fabry <truefurby@gmail.com>
2012-06-16 13:06:33 +0200

    some settings moved to .env

--- checkepisode/settings/external.py
+++ checkepisode/settings/external.py
@@ -1,7 +1,9 @@
-API_KEY = "4E5D5C8EFC4175A1"
+import os
+
+API_KEY = os.environ['CHECKEPISODE_TVDB_API_KEY']
 MIRROR = "http://www.thetvdb.com/"
 REMOVE_OLD = False
 PRINT_DETAIL = True
 REMOVE_OLD_IMAGES = False
-IMAGE_FOLDER = "checkepisode/static/series/"
-LAST_UPDATE = "lastupdate"
+IMAGE_FOLDER = os.environ['CHECKEPISODE_TVDB_IMAGE_FOLDER']
+LAST_UPDATE = os.environ['CHECKEPISODE_TVDB_LASTUPDATE']
